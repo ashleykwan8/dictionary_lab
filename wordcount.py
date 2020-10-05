5b6039731e0fd845db100fe8752d5e56b486cb0f
vagrant <vagrant@vagrant.vm>
2020-10-05 19:43:59 +0000

    first commit

--- wordcount.py
+++ wordcount.py
@@ -1,8 +1,18 @@
 # put your code here.
 
-def letter_count_in_dict(file):
+nFile = open(test.txt)
+ 
+def letter_count_in_dict(nFile):
     ''' Defines count of each letter in dictionary'''
     
+   
     counted_letters = {} #empty dictionary 
 
-    
+    for letter in nFile:
+        if letter.isalpha() == True: 
+            counted_letters[letter] = counted_letters.get(letter, 0) + 1
+
+    return counted_letters
+
+print(letter_count_in_dict(nFile))
+
