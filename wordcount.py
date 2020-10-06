2485f621b5b2d5a3a4cc5455755d63e209c3487f
vagrant <vagrant@vagrant.vm>
2020-10-06 00:42:45 +0000

    finished lab

--- wordcount.py
+++ wordcount.py
@@ -1,18 +1,25 @@
 # put your code here.
-
-nFile = open(test.txt)
  
-def letter_count_in_dict(nFile):
+def word_count_in_dict(file):
     ''' Defines count of each letter in dictionary'''
     
-   
-    counted_letters = {} #empty dictionary 
+    nFile = open(file)
+    counted_words= {} #empty dictionary 
+    
+    for line in nFile: ## for line in nFile:
+        line = line.split(" ")
+        # counted_letters[letter] = counted_letters.get(letter, 0) + 1
 
-    for letter in nFile:
-        if letter.isalpha() == True: 
-            counted_letters[letter] = counted_letters.get(letter, 0) + 1
+        for word in line:
+            if word.isalpha() == True: 
+                counted_words[word] = counted_words.get(word, 0) + 1
+    
+    return sorted(counted_words)
+                
 
-    return counted_letters
+    # return counted_words
 
-print(letter_count_in_dict(nFile))
+print(word_count_in_dict('twain.txt'))
+
+
 
